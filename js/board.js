ffde030d90510462b92c96402e94f58ac9559255
= <jefftam23@gmail.com>
2017-03-01 18:23:25 -0500

    Populate game board with single letter and style board

--- js/board.js
+++ js/board.js
@@ -1,7 +1,30 @@
 class Board {
   constructor($el) {
     this.$el = $el;
-    $el.html("It's working!");
+
+    this.setup();
+  }
+
+  resetBoard() {
+    let randomLetter = "J";
+    $("#board span").html(randomLetter);
+  }
+
+  setup() {
+    const $ul = $("<ul>");
+
+    for (let rowIdx = 0; rowIdx < 4; rowIdx++) {
+      for (let colIdx = 0; colIdx < 4; colIdx++) {
+        let $li = $("<li>");
+        $li.data("pos", [rowIdx, colIdx]);
+        let $span = $("<span class='noselect'></span>");
+        $li.append($span);
+        $ul.append($li);
+      }
+    }
+
+    this.$el.append($ul);
+    this.resetBoard();
   }
 }
 
