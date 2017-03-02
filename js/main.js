9003649f1ecf31d7df9574f9bb974fca89a0612b
= <jefftam23@gmail.com>
2017-03-02 15:16:00 -0500

    Add AJAX request for dictionary

--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@ const Board = require("./board.js");
 const CurrentSelection = require("./currentSelection.js");
 
 $( () => {
-  const $currentSelectionEl = $("#current-selection");
-  const currSelection = new CurrentSelection($currentSelectionEl);
+  $.ajax({
+    method: "GET",
+    url: "dict.txt"
+  }).then( txt => {
+    const words = txt.split("\n");
+    words.pop(); // get rid of empty string entry
+    const dict = {};
+    words.forEach(word => { dict[word] = true; });
 
-  const $boardEl = $("#board");
-  new Board($boardEl, currSelection);
+    const $currentSelectionEl = $("#current-selection");
+    const currSelection = new CurrentSelection($currentSelectionEl);
+
+    const $boardEl = $("#board");
+    new Board($boardEl, currSelection, dict);
+  });
 });
