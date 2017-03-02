3dddd57da34ea273445a5d6f7d7aec7095dc93e2
= <jefftam23@gmail.com>
2017-03-02 09:36:42 -0500

    Handle word selection via mouse input, without necessary checks

--- js/board.js
+++ js/board.js
@@ -1,8 +1,58 @@
 class Board {
   constructor($el) {
     this.$el = $el;
+    this.userSelecting = false;
+    this.posSelections = [];
+    this.letterSelections = "";
+
+    $("body")
+      .on("mouseup mouseleave", this.handleWordSubmission.bind(this));
+
+    this.handleTileMousedown = this.handleTileMousedown.bind(this);
+    this.handleTileMouseenter = this.handleTileMouseenter.bind(this);
 
     this.setup();
+  }
+
+  addTileEventListeners() {
+    const $tiles = $("#board li");
+
+    $tiles
+      .on("mousedown", this.handleTileMousedown)
+      .on("mouseenter", this.handleTileMouseenter);
+  }
+
+  handleWordSubmission(e) {
+    e.preventDefault();
+
+    if (this.userSelecting) {
+      this.userSelecting = false;
+      // 'submit' this.letterSelections
+      console.log(this.letterSelections);
+      this.resetSelections();
+    }
+  }
+
+  handleTileMousedown(e) {
+    e.preventDefault();
+    this.userSelecting = true;
+    this.posSelections.push($(e.currentTarget).data().pos);
+    this.letterSelections += e.currentTarget.children[0].innerHTML;
+  }
+
+  handleTileMouseenter(e) {
+    e.preventDefault();
+    // if:
+    //  1) this.userSelecting === true
+    //  2) not previously selected AND
+    //  3) adjacent to previous selection AND
+    // then:
+    //  push tile pos onto this.posSelections
+
+    if (this.userSelecting) {
+      this.posSelections.push($(e.currentTarget).data().pos);
+      this.letterSelections += e.currentTarget.children[0].innerHTML;
+    }
   }
 
   randomLetter() {
@@ -14,6 +64,11 @@ class Board {
     $("#board span").each((idx, span) => {
       $(span).html(this.randomLetter());
     });
+  }
+
+  resetSelections() {
+    this.posSelections = [];
+    this.letterSelections = "";
   }
 
   setup() {
@@ -31,6 +86,7 @@ class Board {
 
     this.$el.append($ul);
     this.randomizeBoard();
+    this.addTileEventListeners();
   }
 }
 
